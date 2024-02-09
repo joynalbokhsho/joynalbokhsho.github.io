a737f193179733c903145a91a0e3305e2d7c6913
Joynal Bokhsho <shaownsilva@gmail.com>
2024-02-09 20:22:24 +0600

    default theme changes

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -188,7 +188,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'dracula',
+    defaultTheme: 'halloween',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
